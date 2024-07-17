feed024b9952762c194778d81f47bb43b3f3efe0
HafidzThufail22 <hafidzthufail22@gmail.com>
2024-07-17 10:38:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+//read more
+const button = document.quarySelector(".button");
+//
+document.quareSelector("#read-more").onclick = () => {
+  button.classList, toggle("active");
+};
